710193c861cc633a89b991aa5c70ea5a7c80f955
WhatDidIDoWrong <benhindes@gmail.com>
2022-01-20 04:18:35 +0000

    cool text

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("test")
+
+
+#cool text
+import sys
+import time
+def cool_print(text, delay=0.5):
+    sys.stdout.write(t)
+    sys.stdout.flush()
+    time.sleep(delay)
+    print(end="")
+
+def pinput(text):
+
+    printy(text)
+    return input()
+
+
+#intro
+hello = input("say hello: ")
+cool_print(hello)
+print("who told you to say hello stop talking")
+#begining
+userint = input("...")
+
+if userint == "1":
+print("you say nothing akward silence insues")
+
+else:
+print("no")
+print("type 1 for: no response type 2 for: no")
+
+
